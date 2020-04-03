7e97d3b0ff10247e2a855f7ae9e4332093b6bb61
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-04-04 00:25:01 +0900

    Add imageAction.js to index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,13 @@
 import counterActions from "./counterActions";
 import userActions from "./userActions";
 import popularMoviesActions from "./popularActions";
+import imageActions from "./imageAction";
 
 const allActions = {
   counterActions,
   userActions,
-  popularMoviesActions
+  popularMoviesActions,
+  imageActions
 };
 
 export default allActions;
